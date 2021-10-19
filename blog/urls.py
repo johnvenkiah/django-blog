19da37841b3807f5a75a97778ba22ea94484478b
johnvenkiah <contact@johnvenkiah.com>
2021-10-19 16:13:41 +0000

    add post_detail path to urls.py in blog dir, add more content from py files in post_detail.html, add link to post in index.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    # '<slug:slug>/' below = django slug path converter and slug in PostDetail get method
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
