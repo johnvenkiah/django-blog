3b13464d6b436258bc2ef805bf91c99ce1949a16
johnvenkiah <contact@johnvenkiah.com>
2021-10-19 14:33:18 +0000

    add base, index and post_detail templates and populate them with html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class Postlist(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6  # Posts per page, dj auto adds new page if more posts
